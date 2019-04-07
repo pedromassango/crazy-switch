0b77b0eba73a95378ec1267b167d52e9808f6fcc
pedromassango <pedromassango.developer@gmail.com>
2019-04-07 08:51:41 +0100

    created Switch widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Crazy Switch",
-      theme: ThemeData(
-        primarySwatch: Colors.orange
-      ),
+      theme: ThemeData(primarySwatch: Colors.orange),
       home: HomePage(),
     );
   }
@@ -18,7 +16,52 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Crazy Switch"),
+      ),
+      body: Container(
+        child: Center(
+          child: CrazySwitch(),
+        ),
+      ),
+    );
   }
 }
 
+class CrazySwitch extends StatefulWidget {
+  @override
+  _CrazySwitchState createState() => _CrazySwitchState();
+}
+
+class _CrazySwitchState extends State<CrazySwitch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      height: 50,
+      padding: EdgeInsets.fromLTRB(2, 8, 2, 8),
+      decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+      ),
+      child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
